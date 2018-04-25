b9e5e68e46e2c51d4c5cc79ef7f38aabd702d6e4
Sidorov, Dmitry <dsidorov@scnsoft.com>
2018-04-25 17:14:36 +0300

    set old ver

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ import (
 var uniqushPushConfFlags = flag.String("config", "/etc/uniqush/uniqush-push.conf", "Config file path")
 var uniqushPushShowVersionFlag = flag.Bool("version", false, "Version info")
 
-var uniqushPushVersion = "uniqush-push 2.5.1-alfa"
+var uniqushPushVersion = "uniqush-push 2.5.0"
 
 func installPushServices() {
 	srv.InstallGCM()
